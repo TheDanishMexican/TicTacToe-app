30e709e0979418da0a80c61599f893f11c49a234
TheDanishMexican <danieljchristjansen@gmail.com>
2023-10-16 17:18:20 +0200

    finished

--- index.js
+++ index.js
@@ -59,4 +59,67 @@ function addGo(event) {
     }
 
     square.removeEventListener('click', addGo);
+
+    checkScore();
+}
+
+function checkScore() {
+    const allSquares = document.querySelectorAll(".square");
+
+    const winningCombos = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ];
+
+    winningCombos.forEach(combination => {
+        let circleIsWinner = true;
+        let crossIsWinner = true;
+
+        combination.forEach(squareIndex => {
+            const square = allSquares[squareIndex];
+            const cross = square.querySelector('.cross');
+
+            if (!cross) {
+                crossIsWinner = false;
+                return;
+            }
+        });
+
+        combination.forEach(squareIndex => {
+            const square = allSquares[squareIndex];
+            const circle = square.querySelector('.circle');
+
+            if (!circle) {
+                circleIsWinner = false;
+                return;
+            }
+        });
+
+        if (circleIsWinner) {
+            infoDisplay.innerHTML = /*HTML*/ `<article>CIRCLE WINS !!!<br><button class='reset-button'>Reset game</button></article>`;
+
+            document.querySelector(".reset-button").addEventListener("click", resetGameBoard);
+        }
+
+        if (crossIsWinner) {
+            infoDisplay.innerHTML = /*HTML*/ `<article>CROSS WINS !!!</article><button class='reset-button'>Reset game</button>`;
+
+            document.querySelector(".reset-button").addEventListener("click", resetGameBoard);
+        }
+
+    });
+
+    // winningCombos.forEach(combination => look at each square in the combination and see if has an elementchild with the class circle if it does, return the innerHTML = <p>Circle wins</p>)
+}
+
+function resetGameBoard() {
+    const allSquares = document.querySelectorAll(".square");
+
+    allSquares.forEach(square => {
+        square.innerHTML = '';
+    });
+
+    infoDisplay.innerHTML = /*html*/
+        `<article class="info">Circle goes first</article>`;
 }
